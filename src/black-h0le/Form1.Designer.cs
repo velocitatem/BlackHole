4ed572a1857a0706ddc91121141b444fa5d7208d
Daniel Rosel <danalves24@outlook.com>
2020-03-05 17:05:59 +0100

    upload1

---
+++ src/black-h0le/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace black_h0le
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser2.Location = new System.Drawing.Point(12, 81);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(1101, 421);
+            this.webBrowser2.TabIndex = 1;
+            this.webBrowser2.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
+            this.textBox1.Location = new System.Drawing.Point(12, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(754, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(772, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 26);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(12, 41);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(4, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 20);
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(898, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "it is not the best browser \r\never but its anonymous";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1117, 505);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Black h0le";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
---
+++ src/black-h0le/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace black_h0le
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String wURL = textBox1.Text;
+            String UserIn = textBox1.Text;
+            //item detaction and filter
+            if (wURL.Contains("https://") || wURL.Contains("http://"))
+            {
+                wURL = textBox1.Text;
+            }
+            else if (!wURL.Contains(".com") || !wURL.Contains(".cz") || !wURL.Contains(".org") || !wURL.Contains(".eu"))
+            {
+                wURL = "https://google.com/search?q=" + textBox1.Text;
+            }
+            else
+            {
+                wURL = "https://"+textBox1.Text;
+            }
+            webBrowser2.Url = new System.Uri(wURL);
+            //tabs 
+            ToolStripButton tabButton = new ToolStripButton();
+            tabButton.Text = UserIn;
+            tabButton.Click += new EventHandler(this.tabButton_Click);
+            toolStrip1.Items.Add(tabButton);
+        }
+        //menu click
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser2.ScriptErrorsSuppressed = true;
+            helpProvider1.SetShowHelp(textBox1, true);
+            helpProvider1.SetHelpString(textBox1, "Enter a valid text here.");
+        }
+        //button menu click handle
+        private void tabButton_Click(object sender, EventArgs e)
+        {
+            ToolStripItem item = (ToolStripItem)sender;
+            String TabQry = item.Text;
+            String BrowserURl = "";
+            if (TabQry.Contains("https://") || TabQry.Contains("http://"))
+            {
+                BrowserURl = TabQry;
+            }
+            else if (!TabQry.Contains(".com") || !TabQry.Contains(".cz") || !TabQry.Contains(".org") || !TabQry.Contains(".eu"))
+            {
+                BrowserURl = "https://google.com/search?q=" + TabQry;
+            }
+            else
+            {
+                BrowserURl = "https://" + TabQry;
+            }
+
+            webBrowser2.Url = new System.Uri(BrowserURl);
+
+        }
+        
+        private void toolStripButton_Click(object senderm, EventArgs e)
+        {
+            MessageBox.Show("clickd");
+        }
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
